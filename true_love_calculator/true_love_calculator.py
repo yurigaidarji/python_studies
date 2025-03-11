4d4707a4b7f10bf5bd3bde1cd685afb74a98a303
Yuri Gaidarji <yurigaidarji@gmail.com>
2025-03-11 18:43:47 -0300

    adding comments and credits

--- true_love_calculator/true_love_calculator.py
+++ true_love_calculator/true_love_calculator.py
@@ -1,3 +1,10 @@
+# The rules are:
+# write a function called calculate_love_score() that tests the compatibility between two names.  To work out the love score between two people:
+# 1. Take both people's names and check for the number of times the letters in the word TRUE occurs.
+# 2. Then check for the number of times the letters in the word LOVE occurs.
+# 3. Then combine these numbers to make a 2 digit number and print it out.  
+# credits to 100-days-of-code from Dr. Angela Yu
+
 def calculate_love_score(name1, name2):
     w_true = "TRUE"
     w_love = "LOVE"
