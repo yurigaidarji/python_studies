4fbf782e92fc1561ef02f663d5550b7a2a3e7969
Yuri Gaidarji <yurigaidarji@gmail.com>
2025-03-14 17:58:48 -0300

    Adding nested values and accessing them

--- dictionary_tests/manipulations.py
+++ dictionary_tests/manipulations.py
@@ -21,3 +21,29 @@ for thing in programming_dictionary:
 # Wiping dictionary
 programming_dictionary = {}
 print(programming_dictionary)
+
+# Accessing nested values
+travel_log = {
+    "France": ["Paris", "Lille", "Dijon"],
+    "Germany": ["Stuttgart", "Berlin"],
+}
+
+#printing a list value inside an dictionary
+print(travel_log["France"][1])
+
+#printing a list value inside a list
+nested_list = ["A", "B", ["C", "D"]]
+print(nested_list[2][1])
+
+travel_log2 = {
+  "France": {
+    "cities_visited": ["Paris", "Lille", "Dijon"],
+    "total_visits": 12
+   },
+  "Germany": {
+    "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+    "total_visits": 5
+   },
+}
+#printing Stuttgart
+print(travel_log2["Germany"]["cities_visited"][2])
